b4e5f2176f88d9c08a48bc1ab62ad1047f9a631f
João Amendola <mx.joaoo@gmail.com>
2021-10-28 21:10:47 -0300

    Added Post, Put and Delete to PizzaController.cs

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -29,10 +29,41 @@ namespace ContosoPizza.Controllers
             return pizza;
         }
 
-        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {            
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
 
-        // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
 
-        // DELETE action
+            var existingPizza = PizzaService.Get(id);
+            if(existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);           
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if(existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);           
+
+            return NoContent();
+        }
     }
 }
